bb6ee28ffa5d9e61ea71fdb29abf6a67b0a49150
Max Felker <max@bigroomstudios.com>
2016-07-01 16:34:00 -0400

    Added flight plan animations

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$.fn.extend({
+  animateCss: function (animationName, callback) {
+      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+          $(this).removeClass('animated ' + animationName);
+          if(callback) {
+            return callback();
+          }
+      });
+  }
+});
