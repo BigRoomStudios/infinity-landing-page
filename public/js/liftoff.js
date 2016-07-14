687393e052c675b0ec75598aedc46e1198b63396
jdonisvitch <jared@bigroomstudios.com>
2016-07-14 10:37:31 -0400

    more tweaks, work on thank you page

--- public/js/liftoff.js
+++ public/js/liftoff.js
@@ -1,7 +1,7 @@
 
   function playExitAnimation() {
     $('.text, .clouds').addClass('animated zoomOut');
-    $('.rocket').addClass('.shoot');
+    $('.rocket').addClass('animated zoomOutUp');
     setTimeout(function() {
       window.location = '/flightplan';
     },700);
--- public/js/thankyou.js
+++ public/js/thankyou.js
@@ -1,4 +1,27 @@
-$(function(){setTimeout(function() {
-  window.location.href = 'http://bigroomstudios.com';
-}, 10000);
+$(function(){
+  'use-scrict';
+  var seconds;
+  var temp;
+
+ function countdown() {
+   seconds = document.getElementById('countdown').innerHTML;
+   seconds = parseInt(seconds, 10);
+   if (seconds == 1) {
+    $('.container').addClass('animated zoomOut');
+     temp = document.getElementById('countdown');
+     temp.innerHTML = "0";
+     return;
+   }
+   seconds--;
+   temp = document.getElementById('countdown');
+   temp.innerHTML = seconds;
+   addTimeout= setTimeout(countdown, 1000);
+ }
+
+ countdown();
+
+  setTimeout(function() {
+
+    window.location.href = 'http://bigroomstudios.com';
+  }, 10000);
 });
